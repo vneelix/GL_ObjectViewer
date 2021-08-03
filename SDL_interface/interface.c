2d0322a6fbf4605e6067b161717460bc4d4475c2
Vision Neelix <vneelix@oa-b3.msk.21-school.ru>
2021-08-04 00:04:05 +0300

    update interface: update scroll widget

--- SDL_interface/interface.c
+++ SDL_interface/interface.c
@@ -6,7 +6,7 @@
 /*   By: vneelix <vneelix@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 15:18:25 by vneelix           #+#    #+#             */
-/*   Updated: 2021/08/02 23:48:17 by vneelix          ###   ########.fr       */
+/*   Updated: 2021/08/04 00:02:45 by vneelix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,158 +46,16 @@ t_ife	*ife_init(unsigned int width,
 	if (TTF_Init() != 0)
 		return (ife_cleanup_callback((void *)ife));
 	ife->font = TTF_OpenFont("interface_font.ttf", 64);
-	return (ife);
-}
 
-SDL_Texture	*render_text(t_ife *ife, const char *text, uint32_t *w, uint32_t *h) {
-	SDL_Surface	*surf = TTF_RenderText_Blended(ife->font, text, (SDL_Color){255, 255, 255, 255});
-	if (surf == NULL)
-		return (NULL);
-	if (w != NULL)
-		*w = surf->w;
-	if (h != NULL)
-		*h = surf->h;
-	SDL_Texture *textr = SDL_CreateTextureFromSurface(ife->renderer, surf);
-	SDL_FreeSurface(surf);
-	return (textr);
-}
+	IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
 
-void	*widget_init(t_ife *ife, const char *title, const char **string) {
-	
-	uint32_t	string_count = 0;
-	for (; string[string_count]; string_count++);
-
-	void	*widget = calloc(sizeof(uint32_t) + (sizeof(SDL_Texture *) + sizeof(SDL_Rect) + sizeof(SDL_Color)) * (2 + string_count), 1);
-
-	void *ret = widget;
-
-	*(uint32_t *)widget = 2 + string_count;
-	widget += sizeof(uint32_t);
-
-	/* const */
-	double		widget_win_part = 1. / 6.;
-	double		widget_header_part = 1. / 3.;
-	uint32_t	widget_header_height = 24;
-
-	double		string_box_part = 1. / 1.5;
-	uint32_t	string_box_height = 28;
-
-	uint32_t	string_box_padding = 16;
-	/*	*/
-
-	SDL_Texture **texture_writer = widget;
-	SDL_Rect	*rect_writer = widget + sizeof(SDL_Texture *) * (2 + string_count);
-	SDL_Color	*color_writer = (void *)rect_writer + sizeof(SDL_Rect) * (2 + string_count);
-
-	/* widget write */
-	texture_writer++;
-	*color_writer++ = (SDL_Color){68, 68, 68, 255};
-	*rect_writer++ = (SDL_Rect){ife->width - ife->width * widget_win_part,
-										0, ife->width * widget_win_part, ife->height};
-	/*	*/
-
-	/* widget header write */
-	SDL_Rect *widg = (SDL_Rect *)(rect_writer - 1);
-
-	*texture_writer++ = render_text(ife, title, NULL, NULL);
-	*color_writer++ = (SDL_Color){0, 0, 0, 32};
-	*rect_writer++ = (SDL_Rect){widg->x + (widg->w - widg->w * widget_header_part) / 2.,
-										0, widg->w * widget_header_part, widget_header_height};
+	/*  */
+	SDL_Surface *state = IMG_Load("state.png");
+	ife->state = SDL_CreateTextureFromSurface(ife->renderer, state);
+	SDL_FreeSurface(state);
 	/*  */
 
-	for (uint32_t i = 0; i != string_count; i++) {
-		uint32_t w, h;
-		//*texture_writer++ = render_text(ife, string[i]);
-		*color_writer++ = (SDL_Color){255, 0, 0, 32};
-		*texture_writer++ = render_text(ife, string[i], &w, &h);
-		*rect_writer = (SDL_Rect){widg->x + (widg->w - widg->w * string_box_part) / 2., rect_writer[-1].y + rect_writer[-1].h + string_box_padding, widg->w * string_box_part, string_box_height};
-		rect_writer++;
-	}
-
-	return (ret);
-}
-
-int	render_widget(t_ife *ife, void *widget) {
-	uint32_t i = 0;
-	uint32_t widget_elem_count = *(uint32_t *)widget;
-	widget += sizeof(uint32_t);
-
-	const SDL_Texture	**texture_reader = widget;
-	const SDL_Rect		*rect_reader = (const void *)texture_reader + sizeof(SDL_Texture *) * widget_elem_count;
-	const SDL_Color		*color_reader = (const void *)rect_reader + sizeof(SDL_Rect) * widget_elem_count;
-
-	const SDL_Rect *header = rect_reader + 1;
-
-	while (i != widget_elem_count) {
-		if (i >= 2 && (rect_reader[i].y < (header->y + header->h))) {
-			i++;
-			continue;
-		}
-		SDL_SetRenderDrawColor(ife->renderer, (color_reader + i)->r, (color_reader + i)->g, (color_reader + i)->b, (color_reader + i)->a);
-		SDL_RenderFillRect(ife->renderer, rect_reader + i);
-		SDL_RenderCopy(ife->renderer, texture_reader[i], NULL, rect_reader + i);
-		i++;
-	}
-	return (0);
-}
-
-int	widget_scroll(t_ife *ife, void *widget, int px) {
-	uint32_t i = 0;
-	uint32_t widget_elem_count = *(uint32_t *)widget;
-	widget += sizeof(uint32_t);
-
-	const SDL_Texture	**texture_reader = widget;
-	SDL_Rect			*rect_writer = (void *)texture_reader + sizeof(SDL_Texture *) * widget_elem_count;
-	const SDL_Color		*color_reader = (const void *)rect_writer + sizeof(SDL_Rect) * widget_elem_count;
-
-	rect_writer += 2;
-
-	SDL_Rect *header = rect_writer - 1;
-	SDL_Rect *last_box = rect_writer + widget_elem_count - 2 - 1;
-
-	if (px < 0) {
-		if (!(ife->height <= (last_box->y + last_box->h)))
-			return (0);
-	}
-
-	if (px > 0) {
-		if (!((header->y + header->h) >= rect_writer->y))
-			return (0);
-	}
-
-	while (i != widget_elem_count - 2) {
-		rect_writer[i].y += px;
-		i++;
-	}
-	return (0);
-}
-
-int	widget_change_elem_status(t_ife *ife, void *widget, uint32_t x, uint32_t y) {
-	uint32_t	widget_elem_count = *(uint32_t *)widget;
-	const SDL_Rect *rect_reader = widget + sizeof(uint32_t) + sizeof(SDL_Texture *) * widget_elem_count;
-	if (x > rect_reader->x && x < (rect_reader->x + rect_reader->w))
-	{
-		uint32_t	i = 0;
-
-		SDL_Color	*color_writer = (const void *)rect_reader + sizeof(SDL_Rect) * widget_elem_count;
-
-		rect_reader += 2;
-		color_writer += 2;
-
-		while (i != widget_elem_count - 2) {
-			if ((x >= rect_reader[i].x && x <= (rect_reader[i].x + rect_reader[i].w))
-				&& (y >= rect_reader[i].y && y <= (rect_reader[i].y + rect_reader[i].h)))
-			{
-				if (color_writer[i].r != 255)
-					color_writer[i] = (SDL_Color){255, 0, 0, 32};
-				else
-					color_writer[i] = (SDL_Color){0, 255, 0, 32};
-				break ;
-			}
-			i++;
-		}
-	}
-	return (0);
+	return (ife);
 }
 
 int	loop(t_ife *ife)
@@ -233,11 +91,11 @@ int	loop(t_ife *ife)
 			if (evt.type == SDL_MOUSEBUTTONUP && evt.button.button == SDL_BUTTON_LEFT) {
 				int x, y;
 				SDL_GetMouseState(&x, &y);
-				widget_change_elem_status(ife, widget, x, y);
+				widget_change_elem_status(widget, x, y);
 			}
 			SDL_SetRenderDrawColor(ife->renderer, 0, 0, 0, 255);
 			SDL_RenderClear(ife->renderer);
-			render_widget(ife, widget);
+			widget_render(ife, widget + sizeof(uint32_t), *(uint32_t *)widget);
 			SDL_RenderPresent(ife->renderer);
 		}
 	}
--- SDL_interface/interface.h
+++ SDL_interface/interface.h
@@ -6,7 +6,7 @@
 /*   By: vneelix <vneelix@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 15:18:43 by vneelix           #+#    #+#             */
-/*   Updated: 2021/08/01 16:31:15 by vneelix          ###   ########.fr       */
+/*   Updated: 2021/08/04 00:02:16 by vneelix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,13 @@
 # include <SDL2/SDL_ttf.h>
 # include <SDL2/SDL_image.h>
 
+# define WIDGET_WIN_PART (1. / 6.)
+# define WIDGET_HEADER_PART (1. / 3.)
+# define WIDGET_HEADER_HEIGHT 24
+# define TEXT_BOX_PART (1. / 1.5)
+# define TEXT_BOX_HEIGHT 28
+# define TEXT_BOX_PADDING 16
+
 typedef struct s_ife {
 	__uint16_t		width;
 	__uint16_t		height;
@@ -26,6 +33,13 @@ typedef struct s_ife {
 	SDL_Renderer	*renderer;
 
 	TTF_Font		*font;
+
+	SDL_Texture		*state;
 }	t_ife;
+
+void	*widget_init(t_ife *ife, const char *title, const char **string);
+int	widget_render(t_ife *ife, void **widget, uint32_t widget_elem_count);
+int	widget_change_elem_status(void *widget, int x, int y);
+int	widget_scroll(t_ife *ife, void *widget, int px);
 
 #endif
